ea2aed1c0505a92bd9ef694dad545ea36096e726
Wayne Olson <62842516+wayneoco@users.noreply.github.com>
2023-01-25 06:59:54 -0800

    feat: added all bins related functions

---
+++ backend/src/controllers/bins-controllers.js
@@ -0,0 +1,58 @@
+require("dotenv").config();
+const { v4: uuidv4 } = require("uuid");
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
+
+const getAllBins = (req, res) => {
+  pool.query("SELECT * FROM bins", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+const getBinById = (req, res) => {
+  const binId = req.params.binId;
+
+  pool.query(
+    "SELECT * FROM bins WHERE uuid = $1",
+    [binId],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
+    }
+  );
+};
+
+const createBin = (req, res) => {
+  const id = uuidv4();
+  const date = new Date();
+  console.log(id, date);
+
+  pool.query(
+    "INSERT INTO bins (uuid, createdAt) VALUES ($1, $2)",
+    [id, date],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(201).end();
+    }
+  );
+};
+
+module.exports = {
+  getAllBins,
+  getBinById,
+  createBin,
+};
