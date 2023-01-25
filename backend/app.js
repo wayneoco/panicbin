cb37d493a5a4bb919e066d3188ec153318a30d81
Wayne Olson <62842516+wayneoco@users.noreply.github.com>
2023-01-24 17:13:50 -0800

    dev: initial commit

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const binsControllers = require("./controllers/bins-controllers");
+const requestControllers = require("./controllers/requests-controllers");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET, POST",
+  })
+);
+
+app.get("api/bins", binsControllers.getAllBins);
+app.get("api/bins/:binId", binsControllers.getBinById);
+app.post("api/bins", binsControllers.createBin);
+
+app.get("api/:binId/requests", requestsControllers.getAllRequests);
+app.get("api/:requestId", requestsControllers.getRequestById);
+app.post("api/:binId", requestsControllers.createRequestRecord);
